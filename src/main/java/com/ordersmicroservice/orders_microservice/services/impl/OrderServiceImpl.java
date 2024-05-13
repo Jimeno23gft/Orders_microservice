767e3f4509f093809781dcba2ba9195519432ddc
Ruben Marles <rnrv@gft.com>
2024-05-13 15:59:31 +0200

    Added status unpaid by default and patch just status

--- src/main/java/com/ordersmicroservice/orders_microservice/services/impl/OrderServiceImpl.java
+++ src/main/java/com/ordersmicroservice/orders_microservice/services/impl/OrderServiceImpl.java
@@ -1,13 +1,17 @@
 package com.ordersmicroservice.orders_microservice.services.impl;
 
 import com.ordersmicroservice.orders_microservice.dto.Order;
+import com.ordersmicroservice.orders_microservice.dto.Status;
 import com.ordersmicroservice.orders_microservice.models.OrderEntity;
 import com.ordersmicroservice.orders_microservice.repositories.OrderRepository;
 import com.ordersmicroservice.orders_microservice.services.OrderService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -31,20 +35,21 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderEntity addOrder(OrderEntity order) {
+
+        order.setStatus(Status.UNPAID);
+
         return orderRepository.save(order);
     }
     public OrderEntity patchOrder(Long id, OrderEntity updatedOrder) {
 
         OrderEntity existingOrder = orderRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Order not found with id " + id)); // Or use a more specific exception
+                .orElseThrow(() -> new RuntimeException("Order not found with id " + id));
 
-
-            existingOrder.setUser_id(updatedOrder.getUser_id());
-            existingOrder.setFrom_address(updatedOrder.getFrom_address());
-            existingOrder.setTo_address(updatedOrder.getTo_address());
             existingOrder.setStatus(updatedOrder.getStatus());
-            existingOrder.setDate_ordered(updatedOrder.getDate_ordered());
-            existingOrder.setDate_delivered(updatedOrder.getDate_delivered());
+            if(updatedOrder.getStatus() == Status.DELIVERED){
+                existingOrder.setDate_delivered(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+            }
+
             return orderRepository.save(existingOrder);
     }
 
