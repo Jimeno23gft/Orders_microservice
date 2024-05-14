7d8df63ea1c3bb04ee8f17c0e9c41e92404cf929
Ruben Marles <rnrv@gft.com>
2024-05-14 16:16:18 +0200

    get improve with products without main

--- src/test/java/com/ordersmicroservice/orders_microservice/OrdersMicroserviceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.ordersmicroservice.orders_microservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrdersMicroserviceApplicationTests {
-
-}
