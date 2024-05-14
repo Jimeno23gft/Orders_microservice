58b0486a2956e35a16e7977c68ca8770d465ce3f
Carlos Jimeno <csjn@gft.com>
2024-05-14 17:19:03 +0200

    test for webClientConfig

--- src/main/java/com/ordersmicroservice/orders_microservice/config/WebClientConfig.java
+++ src/main/java/com/ordersmicroservice/orders_microservice/config/WebClientConfig.java
@@ -1,9 +1,11 @@
 package com.ordersmicroservice.orders_microservice.config;
 
 
+import lombok.Generated;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Configuration
 public class WebClientConfig {
